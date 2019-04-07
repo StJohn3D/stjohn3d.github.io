d7f94840323b3d0b01eabf9041015d280e6cb508
St. John <stjohn3d@outlook.com>
2019-04-07 15:24:17 -0400

    Added react-router - first test with routing on gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Home } from './pages/Home'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h3>StJohn3D</h3>
-          Under Construction
-        </header>
+function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      Under Construction
+    </div>
+  );
+}
+
+export default BasicExample;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>StJohn3D</h3>
+        Under Construction
+      </header>
+    </div>
+  )
+}
