56b4f973ca54dedfc796e70ae0fa75eb9205d2eb
St. John <stjohn3d@outlook.com>
2019-04-06 22:16:26 -0400

    Added Create React App with gh-pages deploy scripts

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>StJohn3D</h3>
+          Under Construction
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
