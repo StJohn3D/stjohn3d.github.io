8db1cd3a5bf4966cf0e640954540c4cedcbb7b81
St. John <stjohn3d@outlook.com>
2019-04-08 00:02:41 -0400

    Added fontawesome and finished the media page

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVimeoSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
 import { Content } from '../components/Content'
 
 export const Media = () => {
   return (
     <Content>
-      Media is under construction...
+      <a href="https://vimeo.com/stjohn3d/videos/appears/sort:date" target="blank">
+        <FontAwesomeIcon icon={faVimeoSquare} /> Vimeo
+      </a>
+      <a href="https://www.youtube.com/channel/UCHUiwM3sIogMjfwaN60Q-8A" target="blank">
+        <FontAwesomeIcon icon={faYoutubeSquare} /> Vimeo
+      </a>
     </Content>
   )
 }
